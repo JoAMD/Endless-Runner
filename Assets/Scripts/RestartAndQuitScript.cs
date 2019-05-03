7efff15207980d4d669472d61908f7bd399ec1d5
JoAMD <joasus12345@gmail.com>
2019-05-03 13:32:27 +0530

    Changed text colour, added final working build

---
+++ Assets/Scripts/RestartAndQuitScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartAndQuitScript : MonoBehaviour
+{
+
+    public HighScore highScoreScript;
+
+    public void restart(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
